bf78ea7ba0dd8a35981097bb93ee2c432dcde02a
Miriam Bissoli <126024992+miriambixso02@users.noreply.github.com>
2023-11-01 15:57:56 +0100

    initial implementation

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useRef,/* useState, useEffect,*/ Suspense } from "react"
+// OrbitControls to move the camera around
+import { OrbitControls } from "@react-three/drei"
+import { Canvas, /* useFrame, useLoader*/ } from "@react-three/fiber"
+// load progress
+import { Html, useProgress } from "@react-three/drei"
+import Model from "./model"
+import { useControls } from 'leva'
+import { Leva } from 'leva'
+
+function Loader() {
+  const { progress } = useProgress()
+  return <Html center>{progress} % loaded</Html>
+}
+
+const hueConfig = {
+  value: 0,
+  min: 0,
+  max: 360,
+  step: 1,
+}
+
+const saturarionConfig = {
+  value: 100,
+  min: 0,
+  max: 100,
+  step: 1,
+}
+
+const lightnessConfig = {
+  value: 50,
+  min: 10,
+  max: 90,
+  step: 1,
+}
+
+export default function App() {
+  const { hue, saturarion, lightness } = useControls({
+    hue: hueConfig,
+    saturarion: saturarionConfig,
+    lightness: lightnessConfig
+  })
+
+  return (
+    <>
+      <Leva collapsed={true} flat={false}></Leva>
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={.9} />
+
+        <Suspense fallback={<Loader />}>
+          <Model hue={hue} saturation={saturarion} lightness={lightness} />
+        </Suspense>
+
+        <OrbitControls />
+      </Canvas>
+    </>
+  )
+}
