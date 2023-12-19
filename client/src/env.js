60b156a499b0caee70753511b7ab5a0047604f1b
Miriam Bissoli <126024992+miriambixso02@users.noreply.github.com>
2023-12-20 00:23:03 +0100

    Remove font

--- client/src/env.js
+++ client/src/env.js
@@ -1,4 +1,5 @@
 export const SERVER_ADDRESS = process.env.NODE_ENV === 'production'
   ? 'https://miriambixso02.github.io/NJORD'
   : 'http://localhost:3000/NJORD'
+export const QR_CODE_BASE_URL = 'https://miriambixso02.github.io/NJORD'
 export const WS_SERVER = 'localhost'
