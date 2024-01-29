e80d564940198cdf4283acf66c030420547a27a5
Miriam Bissoli <126024992+miriambixso02@users.noreply.github.com>
2024-01-29 14:02:44 +0100

    Correct link for gh pages

--- client/src/env.js
+++ client/src/env.js
@@ -2,6 +2,6 @@
 export const WS_SERVER = 'localhost'
 
 export const SERVER_ADDRESS = process.env.NODE_ENV === 'production'
-  ? 'https://njord-salmon.github.io/NJORD/'
+  ? 'https://njord-salmon.github.io/NJORD'
   : `http://${WS_SERVER}:3000/NJORD`
-export const QR_CODE_BASE_URL = 'https://njord-salmon.github.io/NJORD/'
+export const QR_CODE_BASE_URL = 'https://njord-salmon.github.io/NJORD'
